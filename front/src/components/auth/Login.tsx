bb95cbcb9f57cd7feb5b5f121578a404c3f1564e
Joodarbeshim05 <162398776+Joodarbeshim05@users.noreply.github.com>
2026-01-20 12:45:30 +0600

    добавлены страницы register and login

---
+++ front/src/components/auth/Login.tsx
@@ -0,0 +1,34 @@
+const Login = () => {
+  return (
+    <section className="py-10 flex flex-col items-center">
+      <div className="container">
+        <div className="flex flex-col items-center gap-10 justify-center">
+          <h1 className="text-[30px]">Войти в существуещий аккаунт</h1>
+          <div className="flex flex-col items-center gap-5 justify-center">
+            <input
+              style={{
+                border: "2px solid white",
+              }}
+              className="text-[20px] py-1 px-3 rounded-[5px]"
+              type="email"
+              placeholder="Email..."
+            />
+            <input
+              style={{
+                border: "2px solid white",
+              }}
+              className="text-[20px] py-1 px-3 rounded-[5px]"
+              type="password"
+              placeholder="Password..."
+            />
+            <button className="py-1.25 px-7.5 text-[20px] rounded-[5px] bg-[white] text-[black]">
+              Войти
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
